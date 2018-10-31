97e56ae6e5ed81ae3ef2d1c9e5ed92e5b4a04677
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-10-31 17:52:34 -0500

    Dia miercoles 31 oct
    Terminado moviles.css
    Practica proyecto1 con estilos responsive

---
+++ 31-10/front/Practica CSS/proyecto 1/js/javascript.js
@@ -0,0 +1,14 @@
+addEventListener("load", function(){
+	var visible = true;
+	var menu = document.getElementById("menu-img");
+	menu.addEventListener("click", function(){
+		var menuprincipal = document.getElementById("menuprincipal");
+		if(visible){//el icono
+			menuprincipal.style.display = "block";
+			visible = false;
+		}else{
+			menuprincipal.style.display = "none";
+			visible = true;
+		}
+	});
+});
